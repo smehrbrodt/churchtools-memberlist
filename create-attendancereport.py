smehrbrodt/churchtools-memberlist

commit ddf40cbd9f3c52e26dfe3b90a1cc22e9c6420a37

Handle missing attendance data

create-attendancereport.py
@@ -29,6 +29,8 @@ def is_absent(member):
     return member.present == False
 
 def get_two_weeks_absent(meeting_members):
+    if len(meeting_members) != 2:
+        return []
     thisWeekAbsent = set(filter(is_absent, meeting_members[0]))
     lastWeekAbsent = set(filter(is_absent, meeting_members[1]))
     return sorted(thisWeekAbsent.intersection(lastWeekAbsent))
@@ -65,6 +67,11 @@ meeting_members_stats = []
 for nWeek in range(8):
     meeting_date = orig_meeting_date + datetime.timedelta(weeks=-(nWeek))
     meeting = churchtoolsapi.get_group_meeting(args.group_members, meeting_date)
+    if not meeting:
+        continue
+    if not meeting['isCompleted']:
+        print("Mitglieder-Anwesenheit vom {} nicht abgeschlossen.".format(meeting_date.strftime("%Y-%m-%d")))
+        exit(0)
     meeting_members_stats.append(meeting)
     meeting_members.append(churchtoolsapi.get_meeting_members(args.group_members, meeting['id']))
 
@@ -74,6 +81,12 @@ meeting_regular_visitors_stats = []
 for nWeek in range(2):
     meeting_date = orig_meeting_date + datetime.timedelta(weeks=-(nWeek))
     meeting = churchtoolsapi.get_group_meeting(args.group_regular_visitors, meeting_date)
+    if not meeting:
+        continue
+    #if not meeting['isCompleted']:
+    if True:
+        print("Regelmäßige Besucher-Anwesenheit vom {} nicht abgeschlossen.".format(meeting_date.strftime("%Y-%m-%d")))
+        exit(0)
     meeting_regular_visitors_stats.append(meeting)
     meeting_regular_visitors.append(
         churchtoolsapi.get_meeting_members(

churchtoolsapi.py
@@ -222,7 +222,7 @@ def get_group_meeting(group_id, meeting_date):
         limit=1,
         start_date=start_date_str, end_date=end_date_str)
     meetings_in_group = meetings_in_group_result[0]['data']
-    return meetings_in_group[0]
+    return meetings_in_group[0] if meetings_in_group else None
 
 def get_meeting_members(group_id, meeting_id, filter_role_id=None):
     url = ApiBase._site + 'groups/{groupId}/meetings/{meetingId}/members'.format(groupId=group_id, meetingId=meeting_id)

commit 8bb5aaf2a172aa3441d36162015ba5450f26ce93

Fix absent report for members
Broken since ddf40cbd9f3c52e26dfe3b90a1cc22e9c6420a37

create-attendancereport.py
@@ -29,7 +29,7 @@ def is_absent(member):
     return member.present == False
 
 def get_two_weeks_absent(meeting_members):
-    if len(meeting_members) != 2:
+    if len(meeting_members) < 2:
         return []
     thisWeekAbsent = set(filter(is_absent, meeting_members[0]))
     lastWeekAbsent = set(filter(is_absent, meeting_members[1]))
@@ -68,6 +68,8 @@ for nWeek in range(8):
     meeting_date = orig_meeting_date + datetime.timedelta(weeks=-(nWeek))
     meeting = churchtoolsapi.get_group_meeting(args.group_members, meeting_date)
     if not meeting:
+        print("WARNUNG: Kein Treffen für {} gefunden!".format(meeting_date.strftime("%Y-%m-%d")))
+        meeting_members.append([])
         continue
     if not meeting['isCompleted']:
         print("Mitglieder-Anwesenheit vom {} nicht abgeschlossen.".format(meeting_date.strftime("%Y-%m-%d")))