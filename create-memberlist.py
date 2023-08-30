65dcc8fffc716e2d3c4b07884f7ccdafa59bcbcc
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2023-08-30 08:39:37 +0200

    Add role filter

--- create-memberlist.py
+++ create-memberlist.py
@@ -15,6 +15,7 @@ from py3o.template import Template
 # Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--filter-group", help="Filter for group ID")
+parser.add_argument("--filter-role", help="Filter for role ID")
 parser.add_argument("--template", default="template_memberlist.odt", help="custom template file (odt)")
 parser.add_argument("--output", default="memberlist.odt", help="output file (odt)")
 args = parser.parse_args()
@@ -23,7 +24,7 @@ args = parser.parse_args()
 t = Template(args.template, args.output)
 
 # Sort persons by their family
-persons_sorted = churchtoolsapi.get_persons(args.filter_group, include_images=True)
+persons_sorted = churchtoolsapi.get_persons(args.filter_group, args.filter_role, include_images=True)
 
 data = dict(persons=persons_sorted)
 t.render(data)
